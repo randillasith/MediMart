b4f6cb1ca830c6584523dfa81a45100a9b677df1
randillasith <randillasith900@gmail.com>
2026-04-11 10:00:07 +0530

    project initiation

---
+++ src/main/java/org/pgno20/medimart/MediMartApplication.java
@@ -0,0 +1,13 @@
+package org.pgno20.medimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pgno20/medimart/MediMartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pgno20.medimart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
