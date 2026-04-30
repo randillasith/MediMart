f685416629f342c3e931221dbead3e38661fa9fd
randillasith <randillasith900@gmail.com>
2026-04-30 12:18:37 +0530

    use service layer and DTOs for create and list operations

--- src/main/java/org/pgno20/medimart/controller/CategoryController.java
+++ src/main/java/org/pgno20/medimart/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package org.pgno20.medimart.controller;
 
-import org.pgno20.medimart.model.Category;
-import org.pgno20.medimart.repository.CategoryRepository;
+import jakarta.validation.Valid;
+import org.pgno20.medimart.dto.CategoryCreateRequest;
+import org.pgno20.medimart.dto.CategoryResponse;
+import org.pgno20.medimart.service.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,19 +13,19 @@ import java.util.List;
 @RequestMapping("/api/categories")
 public class CategoryController {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
-    public CategoryController(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @PostMapping
-    public ResponseEntity<Category> create(@RequestBody Category c) {
-        return ResponseEntity.ok(categoryRepository.save(c));
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryCreateRequest req) {
+        return ResponseEntity.ok(categoryService.create(req));
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> list() {
-        return ResponseEntity.ok(categoryRepository.findAll());
+    public ResponseEntity<List<CategoryResponse>> list() {
+        return ResponseEntity.ok(categoryService.listAll());
     }
 }
