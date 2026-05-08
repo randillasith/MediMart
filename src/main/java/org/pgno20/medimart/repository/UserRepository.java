6df9c0844a5b68685352c62c185ee25429c55d06
randillasith <randillasith900@gmail.com>
2026-05-08 21:28:06 +0530

    add user registration
    Co-authored-by: IT25103890 <it25103890@my.sliit.lk>
    Signed-off-by: randillasith <randillasith900@gmail.com>

---
+++ src/main/java/org/pgno20/medimart/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.pgno20.medimart.repository;
+
+import org.pgno20.medimart.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
