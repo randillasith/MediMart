fc333cc22714d2387bbfba245bc76e9120a41d78
randillasith <randillasith900@gmail.com>
2026-05-09 10:44:40 +0530

    add password confirmation
    Co-authored-by: IT25103890 <it25103890@my.sliit.lk>
    Signed-off-by: randillasith <randillasith900@gmail.com>

--- src/main/java/org/pgno20/medimart/dto/PasswordUpdateDTO.java
+++ src/main/java/org/pgno20/medimart/dto/PasswordUpdateDTO.java
@@ -1,6 +1,7 @@
 package org.pgno20.medimart.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,9 @@ public class PasswordUpdateDTO {
     private String currentPassword;
 
     @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "New password must be at least 8 characters")
     private String newPassword;
+
+    @NotBlank(message = "Please confirm your new password")
+    private String confirmNewPassword;
 }
