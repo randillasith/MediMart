56fb630dbabd09530466049af04c4dde0e21b068
IT25103884 <IT25103884@my.sliit.lk>
2026-05-14 23:14:43 +0530

    Bugs fix in SupplierRepository.java

--- src/main/java/org/pgno20/medimart/repository/SupplierRepository.java
+++ src/main/java/org/pgno20/medimart/repository/SupplierRepository.java
@@ -2,10 +2,12 @@ package org.pgno20.medimart.repository;
 
 import org.pgno20.medimart.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
     List<Supplier> findByNameContainingIgnoreCase(String name);
+    @Query("SELECT s FROM Supplier s WHERE LOWER(s.medicinesSupplied) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Supplier> findByMedicinesSuppliedContainingIgnoreCase(String keyword);
 }
